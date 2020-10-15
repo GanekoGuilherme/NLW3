c01d00f5663d151c9b441d1ec2cfd956e459d150
GanekoGuilherme <guilhermeganeko@hotmail.com>
2020-10-15 00:47:07 -0300

    Dia 3 Finalização do front.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
-import { readBuilderProgram } from 'typescript';
 import Routes from './routes';
 
 function App() {
